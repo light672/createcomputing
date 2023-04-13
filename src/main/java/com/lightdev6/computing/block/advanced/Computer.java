da6ed02ceffe15ad1b34f20e640c3b5802f2b422
light672 <99850744+light672@users.noreply.github.com>
2023-04-12 20:44:31 -0500

    Checking if the computer can store variables

--- src/main/java/com/lightdev6/computing/block/advanced/Computer.java
+++ src/main/java/com/lightdev6/computing/block/advanced/Computer.java
@@ -1,6 +1,8 @@
 package com.lightdev6.computing.block.advanced;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.contents.LiteralContents;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
@@ -8,7 +10,11 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 
+import java.awt.*;
+
 public class Computer extends Block {
+
+    String playerName;
 
     public Computer(Properties properties) {
         super(properties);
@@ -17,7 +23,10 @@ public class Computer extends Block {
     @Override
     public void stepOn(Level level, BlockPos blockPos, BlockState blockState, Entity entity) {
         if(entity instanceof Player player) {
-            player.jumpFromGround();
+            if (playerName.length() == 0){
+                playerName = player.getDisplayName().getString();
+            }
+            player.sendSystemMessage(Component.literal(playerName));
         }
         super.stepOn(level, blockPos, blockState, entity);
     }
