0bb4ce81e5323030cb5cc787419b5ac61ca46975
light672 <99850744+light672@users.noreply.github.com>
2023-04-13 17:21:47 -0500

    Making the computer get the item in hand (testing writing code before I add the interface)

--- src/main/java/com/lightdev6/computing/block/advanced/Computer.java
+++ src/main/java/com/lightdev6/computing/block/advanced/Computer.java
@@ -2,13 +2,15 @@ package com.lightdev6.computing.block.advanced;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.contents.LiteralContents;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 
 import java.awt.*;
 
@@ -22,12 +24,19 @@ public class Computer extends Block {
 
     @Override
     public void stepOn(Level level, BlockPos blockPos, BlockState blockState, Entity entity) {
-        if(entity instanceof Player player) {
-            if (playerName.length() == 0){
-                playerName = "34";
-            }
-            player.sendSystemMessage(Component.literal(playerName));
-        }
+
         super.stepOn(level, blockPos, blockState, entity);
+    }
+
+    @Override
+    public InteractionResult use(BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand hand, BlockHitResult result) {
+        //called on right click
+        if(!level.isClientSide && hand.equals(InteractionHand.MAIN_HAND)){
+            ItemStack item = player.getMainHandItem();
+            String itemName = item.getHoverName().getString();
+            player.sendSystemMessage(Component.literal(itemName));
+        }
+
+        return super.use(blockState, level, blockPos, player, hand, result);
     }
 }
