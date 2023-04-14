e54e8f47c257bba9541b2c418a06667719503887
light672 <99850744+light672@users.noreply.github.com>
2023-04-13 19:58:34 -0500

    Started working on CScript interpreter

---
+++ src/main/java/com/lightdev6/computing/cscript/Scanner.java
@@ -0,0 +1,160 @@
+package com.lightdev6.computing.cscript;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
+
+    Scanner(String source){
+        this.source = source;
+    }
+    List<Token> scanTokens() {
+        while(!isAtEnd()) {
+            start = current;
+            scanToken();
+        }
+
+        tokens.add(new Token(TokenType.EOF, "", null, line));
+        return tokens;
+    }
+
+    private void scanToken(){
+        char c = advance();
+        switch (c){
+            //Grouping Symbols
+            case '(': addToken(TokenType.LEFT_PAREN); break;
+            case ')': addToken(TokenType.RIGHT_PAREN); break;
+            case '{': addToken(TokenType.LEFT_BRACE); break;
+            case '}': addToken(TokenType.RIGHT_BRACE); break;
+            case ',': addToken(TokenType.COMMA); break;
+            //Operands
+            case '-': addToken(TokenType.MINUS); break;
+            case '+': addToken(TokenType.PLUS); break;
+            case '*': addToken(TokenType.STAR); break;
+            case '/': addToken(TokenType.SLASH); break;
+            case '!': addToken(match('=') ? TokenType.BANG_EQUAL : TokenType.BANG); break;
+            case '=': addToken(match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL); break;
+            case '<': addToken(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS); break;
+            case '>': addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER); break;
+            //Comment
+            case '#': while (peek() != '\n' && !isAtEnd()) advance(); break;
+            //Whitespace
+            case ' ':
+            case '\r':
+            case '\t':
+                break;
+            case '\n': line++; break;
+            //Strings
+            case '"': string(); break;
+            case 'o': if (match('r')) addToken(TokenType.OR); break;
+
+            default:
+                if(isDigit(c)){
+                    number();
+                } else if (isAlpha(c)){
+                    identifier();
+                } else {
+                    //TODO: add an error system
+                    //throw an error "Unexpected Character"
+
+                }
+                break;
+
+
+        }
+
+    }
+
+    private char advance(){
+        return source.charAt(current++);
+    }
+
+    private char peek(){
+        if(!isAtEnd()) return '\0';
+        return source.charAt(current);
+    }
+
+    private char peekNext(){
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
+
+    private boolean match(char expected){
+        if (isAtEnd()) return false;
+        if (source.charAt(current) != expected) return false;
+        current++;
+        return true;
+    }
+
+
+
+    private boolean isAtEnd(){
+        return current >= source.length();
+    }
+
+    private boolean isDigit(char c){
+        return c >= '0' && c <= '9';
+    }
+
+    private boolean isAlpha(char c){
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c){
+        return isAlpha(c) || isDigit(c);
+    }
+
+    private void string(){
+        while(peek() != '"' && !isAtEnd()){
+            if(peek() == '\n') line++;
+            advance();
+        }
+
+        if(isAtEnd()){
+            //TODO: add an error system
+            //throw an error at this line "Unterminated String"
+            return;
+        }
+
+        advance();
+
+        String value = source.substring(start + 1, current - 1);
+        addToken(TokenType.STRING, value);
+    }
+
+    private void number(){
+        while (isDigit(peek())) advance();
+        //look for decimal point
+        if(peek() == '.' && isDigit(peekNext())) {
+            advance();
+
+            while (isDigit(peek())) advance();
+        }
+
+        addToken(TokenType.NUMBER, Double.parseDouble(source.substring(start, current)));
+    }
+
+    private void identifier(){
+        while(isAlphaNumeric(peek())) advance();
+        addToken(TokenType.IDENTIFIER);
+    }
+
+    private void addToken(TokenType type){
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal){
+        String text = source.substring(start, current);
+        tokens.add(new Token(type,text,literal,line));
+    }
+
+
+
+
+}
