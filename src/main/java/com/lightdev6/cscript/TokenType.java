02267ac39a18a36b60486281d25330a74135bc5c
light672 <99850744+light672@users.noreply.github.com>
2023-04-20 19:46:17 -0500

    Added token types for +=, -=, *=, /=, ^, ^=

--- src/main/java/com/lightdev6/cscript/TokenType.java
+++ src/main/java/com/lightdev6/cscript/TokenType.java
@@ -6,12 +6,16 @@ public enum TokenType {
     // Single-character tokens.
     LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
     COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+    CARET,
 
     // One or two character tokens.
     BANG, BANG_EQUAL,
     EQUAL, EQUAL_EQUAL,
     GREATER, GREATER_EQUAL,
     LESS, LESS_EQUAL,
+    PLUS_EQUAL, MINUS_EQUAL,
+    STAR_EQUAL, SLASH_EQUAL,
+    CARET_EQUAL,
 
     // Literals.
     IDENTIFIER, STRING, NUMBER,
